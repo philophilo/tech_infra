2a439d3e9534ecf69b5b9ff24825a18a9001ee51
Philip Kwesiga <phil.kwesiga@gmail.com>
2021-05-10 21:56:20 +0300

    add terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+    type = "string"
+}
+
+variable "aws_state" {
+    type = "string"
+}
+
+variable "aws_credentials" {
+    type = "string"
+}
+
+variable "aza" {
+	type = "string"
+}
+
+variable "azb" {
+	type = "string"
+}
+
+variable "aws_tag" {
+	type = "string"
+}
+
+variable "subnet_public_aza_cidr" {
+	type = "string"
+}
+
+variable "subnet_public_azb_cidr" {
+	type = "string"
+}
+
+variable "subnet_private_aza_cidr" {
+	type = "string"
+}
+
+variable "subnet_private_azb_cidr" {
+	type = "string"
+}
+
+variable "vpc_fullcidr" {
+	type = "string"
+}
+
+variable "key_name" {
+	type = "string"
+}
+
+variable "aws_account" {
+	type = "string"
+}
+
+variable "instance_type" {
+	type = "string"
+}
